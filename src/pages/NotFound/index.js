282d7b1609828b84bcc09a752a3c0747e2ca9332
Jaymin Panchal <jayminpanchal10@gmail.com>
2017-10-01 19:29:44 +0530

    Added Other components

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+        <div>
+            <h1>
+                About Page
+            </h1>
+        </div>
+    );
+  }
+}
+
+export default NotFound;
