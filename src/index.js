15242c3c4665413b39429057d73e2772ea5162fa
Jaymin Panchal <jayminpanchal10@gmail.com>
2017-10-02 04:08:00 +0530

    added reduxt store and modified index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
 import registerServiceWorker from './registerServiceWorker';
 
+import App from './App';
+
+import './index.css';
+
 render(
-      <BrowserRouter history={history}>
-        <App />
-      </BrowserRouter>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Route component={App} />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
 );
-  
+
 registerServiceWorker();
