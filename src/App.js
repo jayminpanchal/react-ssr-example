b09c65e6779a230d6b5e0424da0d26f3824e21a0
Jaymin Panchal <jayminpanchal10@gmail.com>
2017-10-02 00:43:17 +0530

    Simple Router navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import {
+  Route,
+  Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <hr/>
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/contact" component={Contact}/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+render(
+      <BrowserRouter history={history}>
+        <App />
+      </BrowserRouter>,
+    document.getElementById('root')
+  );
+  
+  registerServiceWorker();
