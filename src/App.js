78f40780bfe1d6bda2d231a9bd5e631f47e6b76d
Jaymin Panchal <jayminpanchal10@gmail.com>
2017-10-03 23:09:49 +0530

    Ant Design library with some UI

--- src/App.js
+++ src/App.js
@@ -3,33 +3,46 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import logo from './logo.svg';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 
+const { Header, Footer, Sider, Content } = Layout;
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-        <hr/>
-
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/contact" component={Contact}/>
-      </div>
+        <Layout className="layout">
+            <Header>
+              <div className="logo" style={{ width: 120,float: 'left'}}>
+                <img src={logo} alt="logo" style={{ width: 90}}/>
+              </div>
+              <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
+                <Menu.Item key="1">
+                  <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <Link to="/about">About</Link>
+                </Menu.Item>
+                <Menu.Item key="3">
+                  <Link to="/contact">Contact</Link>
+                </Menu.Item>
+              </Menu>
+            </Header>
+            <Content>
+              <div style={{minHeight:520,background: '#fafafa'}}>
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+              ©2017 Created by Jaymin Panchal
+            </Footer>
+        </Layout>
     );
   }
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import Page from '../../component/Page';
+import { Row, Col } from 'antd';
 
 class Home extends Component {
   render() {
     return (
-        <div>
-            <h1>
-                Home Page
-            </h1>
-        </div>
+      <Page title="Home" id="home"
+        description="This page will have a custom set description">
+        <Row type="flex" justify="space-between" align="middle">
+          <Col span={12} offset={6}>
+            <h2 style={{textAlign:'center'}}>
+              Home Page
+            </h2>
+            <p style={{textAlign:'center'}}>
+                Proident pariatur aute ex dolore duis voluptate mollit nulla. Proident aliqua labore nostrud magna. Aliquip quis duis ea ex aliqua dolor Lorem consequat dolor. Sint ea adipisicing ad eiusmod exercitation esse incididunt quis deserunt nostrud. Fugiat sunt pariatur non reprehenderit sint sit ex. Nisi veniam sint in nostrud nostrud minim minim reprehenderit ipsum id mollit aliqua.
+                Dolore ad velit eiusmod magna veniam duis reprehenderit esse ex Lorem sit consequat consequat ullamco. Labore irure et et culpa ad qui sunt laboris commodo sunt nulla. Non reprehenderit culpa quis culpa enim incididunt mollit velit laboris amet. Consequat veniam esse sint dolore irure. Consequat ut ipsum occaecat ea ut qui ullamco et qui. Incididunt dolore elit nisi reprehenderit nulla reprehenderit.
+            </p>
+          </Col>
+        </Row>
+      </Page>
     );
   }
 }
